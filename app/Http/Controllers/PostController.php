1012a1af9887e7837f65234de1590619626f02f8
yuta007 <yuta062677gmail.com>
2022-10-09 15:31:41 +0900

    ブログ投稿作成処理実行

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -22,5 +22,12 @@ class PostController extends Controller
         return view('posts/create');
     }
     
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+         return redirect('/posts/' . $post->id);
+    }
+    
 }
 
