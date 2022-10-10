e2dec7d7450066bab82ddc0f292a2a69e2d7550f
yuta007 <yuta062677gmail.com>
2022-10-10 17:45:16 +0900

    ブログ投稿作成処理実行時のバリデーション実装

---
+++ app/Http/Requests/Postrequest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class Postrequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'post.title' => 'required|string|max:100',
+            'post.body' => 'required|string|max:4000',
+            ];
+    }
+}
