e117070fe46a7a28037e64165a52c17721ea0fe6
yuta007 <yuta062677gmail.com>
2022-10-11 15:01:54 +0900

    ブログ投稿編集画面作成

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -15,7 +15,10 @@
                     <h3>本文</h3>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
-                 <div class="footer">
+            <div class="edit">
+               <a href="/posts/{{ $post->id }}/edit">edit</a>
+            </div>
+            <div class="footer">
                <a href="/">戻る</a>
             </div>
         </div>
